e1998a923e585ea087ae860ec2f48951ec3d9abe
Michał Doś <93281456+michaaalD@users.noreply.github.com>
2024-03-26 23:40:23 +0100

    Create zad3.cpp

---
+++ zad3.cpp
@@ -0,0 +1,263 @@
+/* Sprawozdanie
+Michal Dos
+ocena 3
+
+wyniki dzialania programu:
+
+
+ata.000: Cmax: 32 czas: 8.927e-06s
+data.001: Cmax: 1286 czas: 0.000128287s
+data.002: Cmax: 1383 czas: 0.000125652s
+data.003: Cmax: 1114 czas: 0.000123298s
+data.004: Cmax: 1336 czas: 0.000127976s
+data.005: Cmax: 1267 czas: 0.000154845s
+data.006: Cmax: 1224 czas: 0.000125522s
+data.007: Cmax: 1265 czas: 0.000120292s
+data.008: Cmax: 1239 czas: 0.000127906s
+data.009: Cmax: 1298 czas: 0.000131562s
+data.010: Cmax: 1124 czas: 0.000232438s
+data.011: Cmax: 1668 czas: 0.000251404s
+data.012: Cmax: 1757 czas: 0.000252887s
+data.013: Cmax: 1573 czas: 0.000268435s
+data.014: Cmax: 1452 czas: 0.000285567s
+data.015: Cmax: 1479 czas: 0.000246545s
+data.016: Cmax: 1485 czas: 0.000249621s
+data.017: Cmax: 1569 czas: 0.000279646s
+data.018: Cmax: 1592 czas: 0.000248699s
+data.019: Cmax: 1660 czas: 0.000320842s
+data.020: Cmax: 1679 czas: 0.000270389s
+data.021: Cmax: 2450 czas: 0.00048803s
+data.022: Cmax: 2194 czas: 0.000586743s
+data.023: Cmax: 2401 czas: 0.000519829s
+data.024: Cmax: 2327 czas: 0.000488802s
+data.025: Cmax: 2401 czas: 0.000502767s
+data.026: Cmax: 2282 czas: 0.000587434s
+data.027: Cmax: 2367 czas: 0.000505613s
+data.028: Cmax: 2317 czas: 0.00050942s
+data.029: Cmax: 2347 czas: 0.000518065s
+data.030: Cmax: 2317 czas: 0.000576383s
+data.031: Cmax: 2733 czas: 0.00328941s
+data.032: Cmax: 2843 czas: 0.00161933s
+data.033: Cmax: 2724 czas: 0.00169564s
+data.034: Cmax: 2819 czas: 0.00162332s
+data.035: Cmax: 2916 czas: 0.00158421s
+data.036: Cmax: 2847 czas: 0.00163717s
+data.037: Cmax: 2810 czas: 0.00166765s
+data.038: Cmax: 2702 czas: 0.00166182s
+data.039: Cmax: 2576 czas: 0.00166959s
+data.040: Cmax: 2794 czas: 0.0015001s
+data.041: Cmax: 3166 czas: 0.00322803s
+data.042: Cmax: 3017 czas: 0.00320461s
+data.043: Cmax: 3099 czas: 0.00610996s
+data.044: Cmax: 3144 czas: 0.00313292s
+data.045: Cmax: 3207 czas: 0.00314315s
+data.046: Cmax: 3181 czas: 0.00322103s
+data.047: Cmax: 3239 czas: 0.00621623s
+data.048: Cmax: 3181 czas: 0.00337248s
+data.049: Cmax: 3048 czas: 0.00321272s
+data.050: Cmax: 3267 czas: 0.00325347s
+data.051: Cmax: 4152 czas: 0.0106935s
+data.052: Cmax: 3992 czas: 0.0105847s
+data.053: Cmax: 3880 czas: 0.0108525s
+data.054: Cmax: 3954 czas: 0.013766s
+data.055: Cmax: 3835 czas: 0.01403s
+data.056: Cmax: 3924 czas: 0.00884605s
+data.057: Cmax: 3978 czas: 0.0105371s
+data.058: Cmax: 3935 czas: 0.00779021s
+data.059: Cmax: 3982 czas: 0.0107301s
+data.060: Cmax: 3993 czas: 0.0129671s
+data.061: Cmax: 5519 czas: 0.0262749s
+data.062: Cmax: 5284 czas: 0.0223449s
+data.063: Cmax: 5237 czas: 0.0199579s
+data.064: Cmax: 5044 czas: 0.0255546s
+data.065: Cmax: 5311 czas: 0.0238535s
+data.066: Cmax: 5188 czas: 0.0251165s
+data.067: Cmax: 5305 czas: 0.0251349s
+data.068: Cmax: 5182 czas: 0.0179376s
+data.069: Cmax: 5504 czas: 0.0220793s
+data.070: Cmax: 5385 czas: 0.0188311s
+data.071: Cmax: 5923 czas: 0.0478906s
+data.072: Cmax: 5439 czas: 0.0471021s
+data.073: Cmax: 5780 czas: 0.0492563s
+data.074: Cmax: 5972 czas: 0.0542243s
+data.075: Cmax: 5683 czas: 0.0427799s
+data.076: Cmax: 5425 czas: 0.0446508s
+data.077: Cmax: 5719 czas: 0.0478437s
+data.078: Cmax: 5800 czas: 0.048643s
+data.079: Cmax: 6034 czas: 0.0501334s
+data.080: Cmax: 5918 czas: 0.046877s
+data.081: Cmax: 6579 czas: 0.103359s
+data.082: Cmax: 6548 czas: 0.104927s
+data.083: Cmax: 6572 czas: 0.102308s
+data.084: Cmax: 6599 czas: 0.109522s
+data.085: Cmax: 6704 czas: 0.106863s
+data.086: Cmax: 6736 czas: 0.105429s
+data.087: Cmax: 6621 czas: 0.107927s
+data.088: Cmax: 6838 czas: 0.0979094s
+data.089: Cmax: 6636 czas: 0.107627s
+data.090: Cmax: 6819 czas: 0.100525s
+data.091: Cmax: 10942 czas: 0.358156s
+data.092: Cmax: 10702 czas: 0.373881s
+data.093: Cmax: 11131 czas: 0.314051s
+data.094: Cmax: 11057 czas: 0.332125s
+data.095: Cmax: 10692 czas: 0.371412s
+data.096: Cmax: 10537 czas: 0.365044s
+data.097: Cmax: 10956 czas: 0.341644s
+data.098: Cmax: 10856 czas: 0.266235s
+data.099: Cmax: 10629 czas: 0.334541s
+data.100: Cmax: 10955 czas: 0.244498s
+data.101: Cmax: 11651 czas: 0.50086s
+data.102: Cmax: 11868 czas: 0.497112s
+data.103: Cmax: 11894 czas: 0.398196s
+data.104: Cmax: 11860 czas: 0.377978s
+data.105: Cmax: 11699 czas: 0.409856s
+data.106: Cmax: 11728 czas: 0.492929s
+data.107: Cmax: 11891 czas: 0.399165s
+data.108: Cmax: 11834 czas: 0.42002s
+data.109: Cmax: 11781 czas: 0.32858s
+data.110: Cmax: 11891 czas: 0.492547s
+data.111: Cmax: 26670 czas: 4.65145s
+data.112: Cmax: 27228 czas: 4.09827s
+data.113: Cmax: 27005 czas: 5.61172s
+data.114: Cmax: 27043 czas: 3.95658s
+data.115: Cmax: 26968 czas: 6.0271s
+data.116: Cmax: 27136 czas: 6.46523s
+data.117: Cmax: 26987 czas: 5.28009s
+data.118: Cmax: 27151 czas: 5.26192s
+data.119: Cmax: 26862 czas: 4.8353s
+data.120: Cmax: 27179 czas: 4.57303s
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+
+/**
+ * @brief 
+    Oblicza długość wykonywania zadań na maszynach.
+ * 
+ * @param N n-te zadanine
+ * @param M dla kazdej maszyny m od 1 do M:
+ * @param P 
+ * @param X 
+ * @return int Zwraca wartosc T[M] długosc wykonywania wszystkich zadan
+ */
+int cmax(int N, int M, int *P, int *X) //funkcja licząca długość wykonywania zadań
+{
+	int T[100];
+	for (int m = 0; m <= M; m++) //Inicjalizuje tablicd T zerami
+	{
+		T[m] = 0;
+	}
+	for (int n = 0; n < N; n++)
+	{
+		for (int m = 1; m <= M; m++)
+		{
+			T[m] = std::max(T[m],T[m-1]) + P[(m-1)+X[n]*M];
+		}
+	}
+	return T[M];
+}
+
+
+/**
+ * @brief 
+    Oblicza wage dla kazdego zadania
+ * 
+ * @param N 
+ * @param M 
+ * @param P 
+ * @param X 
+ */
+void wagi(int N, int M, int* P, int* X)
+{
+	int* W = new int[N]; //tworze dynamicznie tablice W o rozmiarze N
+	for(int c = 0; c < N; c++){	 //przypisanie wagi kazdemu zadaniu
+		for(int d = 0; d < M; d++){
+			W[c] += P[c * M + d]; // Sumuje czasy wykonywania poszczegolnych operacji tego zadania i przypisuje te sume jako wage zadania
+		}
+	}
+	for(int b = 0; b < N - 1; b++){		//sortowanie zadan wagami, sortuje zadania malejaco wedlug wag
+		for (int a = 0; a < N - 1; a++){
+			if(W[a] < W[a + 1]){
+				std::swap(W[a], W[a + 1]); //przestawia odpowiednio zadania w tablicy X
+				std::swap(X[a], X[a + 1]);
+			}
+		}
+	}
+	delete[]W;
+}
+
+
+/**
+ * @brief 
+    Wywołuje funkcje wagi dla danego zestawu danych
+ * 
+ * @param N 
+ * @param M 
+ * @param P 
+ * @param X 
+ * @return int Zwraca czas wykonywania dla ostatecznego uszeregowania
+ */
+int NEH(int N, int M, int* P, int* X){
+	wagi(N,M,P,X);
+	for(int n = 0; n < N; n++){		//wstawianie na odpowiednią pozycję
+		int bestP = -1, bestCmax = 999999999;  //Szuka najlepszej pozycji bestP na wstawienie zadania
+		for(int p = n; p >= 0; p--){   //iteruje po pozycjach p od n do 0
+			int tmp = cmax(n+1, M, P, X);
+			if (bestCmax >= tmp){   //Jesli ten czas jest lepszy niz dotychczasowy najlepszy, aktualizuje bestCmax i bestP
+				bestCmax = tmp;   
+				bestP = p;
+			}
+			if(p){
+				std::swap(X[p], X[p - 1]);
+			}
+		}
+		for(int i = 0; i < bestP; i++){
+			std::swap(X[i], X[i + 1]);  //Przesuwa zadanie na odpowiednia pozycje
+		}
+	}
+	return cmax(N, M, P, X);
+}
+int main()
+{
+	int N, M, P[10000],X[1000];
+	int x;
+	std::string s="data.",string1,string2;
+	std::ifstream f("data.txt");		// otworzenie strumienia wczytywania danych z pliku
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	for(int i = 0; i < 121; i++){
+		if(i < 10){
+			string1 = s + "00" + std::to_string(i) + ":";
+		}
+		else if(i < 100){
+			string1 = s + "0" + std::to_string(i) + ":";
+		}
+		else{
+			string1 = s + std::to_string(i) + ":";
+		}
+		while(string2 != string1){				//szukanie odpowiedniego zbioru danych
+			f >> string2;
+		}
+		f >> N >> M;						//wczytanie ilości zadań oraz ilości maszyn
+		for(int j = 0; j < N * M; j++){		//wczytywanie parametrów kolejnych zadań
+			f >> P[j];
+		}
+		for(int j = 0; j < N; j++){
+			X[j] = j;
+		}
+		
+        auto start1=std::chrono::high_resolution_clock::now();
+		x = NEH(N, M, P, X);
+		auto end1 = std::chrono::high_resolution_clock::now();
+		auto duration1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end1 - start1);
+
+		std::cout << string1 <<" Cmax: "<<x<< " czas: "<<duration1.count() * 1e-9 << "s" << std::endl;
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+	
+	f.close();			//zamknięcie strumienia wczytywania danych z pliku
+}
